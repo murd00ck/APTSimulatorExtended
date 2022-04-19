6a881dd23c9aeacd64a8dfb89c721aa43dba55d4
murd00ck <cesare.dilucchio@protonmail.com>
2022-04-19 17:32:37 +0200

    fixed automated collection

--- test-sets/collection/collection_win_automated_collection.bat
+++ test-sets/collection/collection_win_automated_collection.bat
@@ -1,7 +1,3 @@
-dir 'c:' /b /s .docx | findstr /e .docx
-powershell Get-ChildItem -Recurse -Include *.doc | % {Copy-Item $_.FullName -destination c:\temp}
-
-
 ECHO ===========================================================================
 ECHO Mass automated collection of a file type
 dir 'c:' /b /s .docx | findstr /e .docx
